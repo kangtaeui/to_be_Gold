f3b376287c452b4d27401c8fec5f45372466261b
kangtaeui <141004174+kangtaeui@users.noreply.github.com>
2024-05-16 17:49:32 +0900

    Update BubbleSort.c

--- Tae2/Sort/BubbleSort.c
+++ Tae2/Sort/BubbleSort.c
@@ -10,7 +10,7 @@ int main()
 	int num;
 	printf("버블 정렬을 짜겠습니다\n");
 	printf("숫자 5개를 입력해주세요! :");
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		scanf("%d", &num);
 		Bubble[i] = num;
@@ -19,7 +19,7 @@ int main()
 	for (int i = 0; i < 6; i++)
 	{
 		int temp = 0;
-		for (int j = 0; j < 6 - i - 1; j++)
+		for (int j = 0; j < 5 - i - 1; j++)
 		{
 			if (Bubble[j] > Bubble[j + 1])
 			{
